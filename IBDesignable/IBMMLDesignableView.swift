cb298af8f679c3ff5665b11a52b12d8f72e6e73d
Mukesh Lokare <mukeshlokare555@gmail.com>
2017-08-04 23:38:32 +0530

    Update code

--- IBDesignable/IBMMLDesignableView.swift
+++ IBDesignable/IBMMLDesignableView.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+@IBDesignable
 class IBMMLDesignableView: UIView {
 
     /*
@@ -18,4 +19,33 @@ class IBMMLDesignableView: UIView {
     }
     */
 
+
+    //Corner Radius
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    //Background Color
+    @IBInspectable var backColor: UIColor? {
+        didSet {
+            backgroundColor = backColor
+        }
+    }
+    
+    //Border Width
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    //Border Color
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
 }
