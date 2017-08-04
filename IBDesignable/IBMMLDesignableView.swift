c60f1070bc9b935c2c678097c70f9058c3909816
Mukesh Lokare <mukeshlokare555@gmail.com>
2017-08-04 23:28:16 +0530

    first commit

---
+++ IBDesignable/IBMMLDesignableView.swift
@@ -0,0 +1,21 @@
+//
+//  IBMMLDesignableView.swift
+//  IBDesignable
+//
+//  Created by webwerks on 04/08/17.
+//  Copyright © 2017 smart. All rights reserved.
+//
+
+import UIKit
+
+class IBMMLDesignableView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
